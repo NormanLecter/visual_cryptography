427e0196a06ee529f6a10bcfac005186c2237082
Lukasz Wolniak <lukasz.maksymilian.wolniak@gmail.com>
2018-01-05 21:28:22 +0100

    end working with visual cryptography
    code is done

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ let itForWidth = 0;
 let itForHeight = 0;
 let  decide;
 
-let share1 = new Jimp(100, 100, (err, image) => {
+let share1 = new Jimp(100, 50, (err, image) => {
   if(err){
     console.log(err);
   }
 });
 
-let share2 = new Jimp(100, 100, (err, image) => {
+let share2 = new Jimp(100, 50, (err, image) => {
   if(err){
     console.log(err);
   }
 });
 
-let blackResult = new Jimp(100, 100, (err, image) => {
+let blackResult = new Jimp(100, 50, (err, image) => {
   if(err){
     console.log(err);
   }
@@ -34,7 +34,7 @@ Jimp.read("black.bmp").then( (image) => {
     for(let j = 0; j < height - 1; j++){
       if(image.getPixelColor(i,j) < 1000000){
         decide = Math.floor((Math.random() * 100) + 1); 
-        if(decide <= 33){
+        if(decide <= 16){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
@@ -43,9 +43,18 @@ Jimp.read("black.bmp").then( (image) => {
           share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
           share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
           share2.setPixelColor(0xffffffff, i + itForWidth + 1, j + 1); 
-          itForHeight++;
         }
-        else if(decide > 33 && decide < 66){
+        else if(decide > 16 && decide <= 33){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+        }
+        else if(decide > 33 && decide <= 49){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
           share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
@@ -54,9 +63,18 @@ Jimp.read("black.bmp").then( (image) => {
           share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
           share2.setPixelColor(0xffffffff, i + itForWidth + 1, j + 1); 
-          itForHeight++;
         }
-        else if(decide >= 66){
+        else if(decide > 49 && decide <= 65){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+        }
+        else if(decide > 65 && decide <= 81){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
@@ -65,12 +83,21 @@ Jimp.read("black.bmp").then( (image) => {
           share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
           share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
           share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
-          itForHeight++;
+        }
+        else if(decide > 81){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share1.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j + 1); 
         }
       }
       else{
         decide = Math.floor((Math.random() * 100) + 1); 
-        if(decide <= 33){
+        if(decide <= 16){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
@@ -78,21 +105,39 @@ Jimp.read("black.bmp").then( (image) => {
           share2.setPixelColor(0xffffffff, i + itForWidth, j); 
           share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
-          share2.setPixelColor(0x00000000, i + itForWidth + 1, j  + 1); 
-          itForHeight++;
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
         }
-        else if(decide > 33 && decide < 66){
+        else if(decide > 16 && decide <= 33){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+        }
+        else if(decide > 33 && decide <= 49){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
           share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
-          share1.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+          share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share2.setPixelColor(0xffffffff, i + itForWidth, j); 
           share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
           share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
-          share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
-          itForHeight++;
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
         }
-        else if(decide >= 66){
+        else if(decide > 49 && decide <= 65){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+        }
+        else if(decide > 65 && decide <= 81){
           share1.setPixelColor(0xffffffff, i + itForWidth, j); 
           share1.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share1.setPixelColor(0x00000000, i + itForWidth, j + 1); 
@@ -100,17 +145,28 @@ Jimp.read("black.bmp").then( (image) => {
           share2.setPixelColor(0xffffffff, i + itForWidth, j); 
           share2.setPixelColor(0x00000000, i + itForWidth + 1, j); 
           share2.setPixelColor(0x00000000, i + itForWidth, j + 1); 
-          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j + 1); 
-          itForHeight+2;
-      }
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j + 1); 
+        }
+        else if(decide > 81){
+          share1.setPixelColor(0x00000000, i + itForWidth, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share1.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share1.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+          share2.setPixelColor(0x00000000, i + itForWidth, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth + 1, j); 
+          share2.setPixelColor(0xffffffff, i + itForWidth, j + 1); 
+          share2.setPixelColor(0x00000000, i + itForWidth + 1, j + 1); 
+        }
     }
   }
   itForWidth++;
 }
+
 share1.write("share1.bmp");
 share2.write("share2.bmp");
+
 for(let i = 0; i < 100; i++){
-  for(let j = 0; j < 100; j++){
+  for(let j = 0; j < 50; j++){
   blackResult.setPixelColor(share1.getPixelColor(i,j)-share2.getPixelColor(i,j),i,j);
   }
 }
